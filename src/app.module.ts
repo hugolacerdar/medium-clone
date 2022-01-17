203f38e9c2a90b9061c7a39bf08edf599790a258
Hugo <hugolacerdar@gmail.com>
2022-01-17 19:57:22 -0300

    Add module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { AppController } from '@app/app.controller';
+import { AppService } from '@app/app.service';
+import { TagModule } from '@app/tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from '@app/ormconfig';
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(config), TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
