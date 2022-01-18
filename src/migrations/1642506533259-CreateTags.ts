dc46912aa42561adf852ff5692073d28fddff47b
Hugo <hugolacerdar@gmail.com>
2022-01-18 08:52:55 -0300

    Add tags table migration file

---
+++ src/migrations/1642506533259-CreateTags.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTags1642506533259 implements MigrationInterface {
+  name = 'CreateTags1642506533259';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "tags" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, CONSTRAINT "PK_e7dc17249a1148a1970748eda99" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "tags"`);
+  }
+}
