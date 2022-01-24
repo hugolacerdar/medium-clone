a69d2503f95160a2ee78981ac59d053a9571afd6
root <hugolacerdar@gmail.com>
2022-01-24 13:44:14 -0300

    Add auth middleware

---
+++ src/user/middlewares/auth.middleware.ts
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import { ExpressRequest } from '@app/types/expressRequest.interface';
+import { Injectable, NestMiddleware, Req } from '@nestjs/common';
+import { NextFunction, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import { UserService } from '../user.service';
+import { JWTPayload } from '../types/jwtPayload.interface';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  constructor(private readonly userService: UserService) {}
+
+  async use(req: ExpressRequest, _: Response, next: NextFunction) {
+    if (!req.headers.authorization) {
+      req.user = null;
+      next();
+      return;
+    }
+
+    const token = req.headers.authorization.split(' ')[1];
+
+    try {
+      const decode: JWTPayload = verify(
+        token,
+        process.env.JWT_SECRET,
+      ) as JWTPayload;
+      const user = await this.userService.findById(decode.id);
+      req.user = user;
+      next();
+    } catch (error) {
+      req.user = null;
+      next();
+    }
+  }
+}
