6e9c58c485d09b3b686a5ed0d8a6f77870306e3f
root <hugolacerdar@gmail.com>
2022-01-24 14:56:56 -0300

    Add update user endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,12 +3,14 @@ import {
   Controller,
   Get,
   Post,
+  Put,
   UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { User } from './decorators/user.decorator';
 import { CreateUserDTO } from './dto/createUser.dto';
+import { UpdateUserDTO } from './dto/updateUser.dto';
 import { LoginDTO } from './dto/login.dto';
 import { AuthGuard } from './guards/auth.guard';
 import { UserResponseInterface } from './types/userResponse.interface';
@@ -43,5 +45,19 @@ export class UserController {
   @UseGuards(AuthGuard)
   async currentUser(@User() user: UserEntity): Promise<UserResponseInterface> {
     return this.userService.buildUserResponse(user);
+  }
+
+  @Put('user')
+  @UseGuards(AuthGuard)
+  @UsePipes(new ValidationPipe())
+  async updateCurrentUser(
+    @User() currentUser,
+    @Body('user') updateUserDTO: UpdateUserDTO,
+  ): Promise<UserResponseInterface> {
+    const updatedUser = await this.userService.update(
+      currentUser,
+      updateUserDTO,
+    );
+    return this.userService.buildUserResponse(updatedUser);
   }
 }
