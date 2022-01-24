67e607cae6ca44cea801a0d25a125d62b8c7fec0
root <hugolacerdar@gmail.com>
2022-01-24 13:40:59 -0300

    Add GET current user endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,16 @@
+import { ExpressRequest } from '@app/types/expressRequest.interface';
 import {
   Body,
   Controller,
+  Get,
   Post,
+  Req,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateUserDTO } from './dto/createUser.dto';
+import { LoginDTO } from './dto/login.dto';
 import { UserResponseInterface } from './types/userResponse.interface';
-import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
 
 @Controller()
@@ -22,5 +25,22 @@ export class UserController {
     const user = await this.userService.createUser(createUserDTO);
 
     return this.userService.buildUserResponse(user);
+  }
+
+  @Post('users/login')
+  @UsePipes(new ValidationPipe())
+  async login(
+    @Body('user') loginDTO: LoginDTO,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.login(loginDTO);
+
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Get('user')
+  async currentUser(
+    @Req() request: ExpressRequest,
+  ): Promise<UserResponseInterface> {
+    return this.userService.buildUserResponse(request.user);
   }
 }
