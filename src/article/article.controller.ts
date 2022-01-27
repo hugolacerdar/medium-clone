b1c5798a9303677b6f2689e60fedf4c8e36754ff
root <hugolacerdar@gmail.com>
2022-01-27 15:40:45 -0300

    Add find all articles and update article endpoints

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -8,16 +8,29 @@ import {
   Get,
   Param,
   Post,
+  Put,
+  Query,
   UseGuards,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { DeleteResult } from 'typeorm';
 import { ArticleService } from './article.service';
 import { CreateArticleDTO } from './dto/createArticle.dto';
 import { ArticleResponseInterface } from './types/articleResponse.interface';
+import { ArticlesResponseInterface } from './types/articlesResponse.interface';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
+
+  @Get()
+  async findAll(
+    @User('id') currentUserId: string,
+    @Query() query: any,
+  ): Promise<ArticlesResponseInterface> {
+    return await this.articleService.findAll(currentUserId, query);
+  }
 
   @Get(':slug')
   async findBySlug(@Param('slug') slug: string) {
@@ -47,5 +60,22 @@ export class ArticleController {
     @Param('slug') slug: string,
   ): Promise<DeleteResult> {
     return await this.articleService.deleteArticle(currentUserId, slug);
+  }
+
+  @Put(':slug')
+  @UseGuards(AuthGuard)
+  @UsePipes(new ValidationPipe())
+  async update(
+    @User('id') currentUserId: string,
+    @Param('slug') slug: string,
+    @Body('article') updateArticleDTO: CreateArticleDTO,
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articleService.updateArticle(
+      currentUserId,
+      slug,
+      updateArticleDTO,
+    );
+
+    return await this.articleService.buildArticleResponse(article);
   }
 }
