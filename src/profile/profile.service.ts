c1a6f088c3333dd85a2aa90a13056f5f16256965
root <hugolacerdar@gmail.com>
2022-01-28 13:35:59 -0300

    Add follow service

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -2,6 +2,7 @@ import { UserEntity } from '@app/user/user.entity';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { FollowEntity } from './follow.entity';
 import { ProfileType } from './types/profile.type';
 
 @Injectable()
@@ -9,6 +10,8 @@ export class ProfileService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
   ) {}
 
   async find(
@@ -18,14 +21,54 @@ export class ProfileService {
     const user = await this.userRepository.findOne({
       username: profileUsername,
     });
-    console.log(user);
 
     if (!user) {
       throw new HttpException('Profile does not exist.', HttpStatus.NOT_FOUND);
     }
 
-    const profile = { ...user, following: false } as ProfileType;
+    const isFollowing = !!(await this.followRepository.findOne({
+      followerId: currentUserId,
+      followingId: user.id,
+    }));
+
+    const profile = { ...user, following: isFollowing };
     return profile;
+  }
+
+  async follow(
+    currentUserId: string,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      username: profileUsername,
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exist.', HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        `Follower and Following can't be equal.`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const follow = await this.followRepository.findOne({
+      followerId: currentUserId,
+      followingId: user.id,
+    });
+
+    if (!follow) {
+      const followToCreate = new FollowEntity();
+
+      followToCreate.followingId = user.id;
+      followToCreate.followerId = currentUserId;
+
+      await this.followRepository.save(followToCreate);
+    }
+
+    return { ...user, following: true };
   }
 
   buildProfileResponse(profile: ProfileType) {
