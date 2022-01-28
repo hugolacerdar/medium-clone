8b2f5681f24f71e898abd0dfc3a0a014a6e55aee
root <hugolacerdar@gmail.com>
2022-01-28 13:36:26 -0300

    Add follow endpoint

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,5 +1,6 @@
 import { User } from '@app/user/decorators/user.decorator';
-import { Controller, Get, Param } from '@nestjs/common';
+import { AuthGuard } from '@app/user/guards/auth.guard';
+import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileResponseInterface } from './types/profileResponse.interface';
 
@@ -13,6 +14,20 @@ export class ProfileController {
     @Param('username') profileUsername: string,
   ): Promise<ProfileResponseInterface> {
     const profile = await this.profileService.find(
+      currentUserId,
+      profileUsername,
+    );
+
+    return this.profileService.buildProfileResponse(profile);
+  }
+
+  @Post(':username/follow')
+  @UseGuards(AuthGuard)
+  async followProfile(
+    @User('id') currentUserId: string,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.follow(
       currentUserId,
       profileUsername,
     );
