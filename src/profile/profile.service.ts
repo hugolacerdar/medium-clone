8ef0adb3ca079c57f058a7eeb428c7b4c43bc4ae
root <hugolacerdar@gmail.com>
2022-01-28 13:44:53 -0300

    Add unfollow service

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -71,6 +71,33 @@ export class ProfileService {
     return { ...user, following: true };
   }
 
+  async unfollow(
+    currentUserId: string,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      username: profileUsername,
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exist.', HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        `Follower and Following can't be equal.`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    await this.followRepository.delete({
+      followerId: currentUserId,
+      followingId: user.id,
+    });
+
+    return { ...user, following: false };
+  }
+
   buildProfileResponse(profile: ProfileType) {
     delete profile.email;
     return { profile };
