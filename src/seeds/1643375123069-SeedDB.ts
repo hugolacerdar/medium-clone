e5ee6f65cbf205b5bf2c3694a7c337f687770bc1
root <hugolacerdar@gmail.com>
2022-01-28 11:11:31 -0300

    Fix down method warning

--- src/seeds/1643375123069-SeedDB.ts
+++ src/seeds/1643375123069-SeedDB.ts
@@ -23,6 +23,8 @@ export class SeedDB1643375123069 implements MigrationInterface {
     );
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  public async down(_queryRunner: QueryRunner): Promise<void> {}
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public async down(_queryRunner: QueryRunner): Promise<void> {
+    throw new Error('Down method not implemented');
+  }
 }
